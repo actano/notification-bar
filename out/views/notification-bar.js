f5c3a0addb82bbfa72f274c4c4bc11d248901288
Andreas Lubbe <git@lubbe.org>
2015-06-16 11:55:25 +0200

    release 0.2.0

--- out/views/notification-bar.js
+++ out/views/notification-bar.js
@@ -4,19 +4,22 @@ try {
 var buf = [];
 var jade_mixins = {};
 var jade_interp;
-;var locals_for_with = (locals || {});(function (maxWidth, message) {
+;var locals_for_with = (locals || {});(function (message) {
 jade_debug.unshift(new jade.DebugItem( 0, jade_debug[0].filename ));
 jade_debug.unshift(new jade.DebugItem( 1, jade_debug[0].filename ));
 buf.push("<div id=\"notification-bar\">");
 jade_debug.unshift(new jade.DebugItem( undefined, jade_debug[0].filename ));
 jade_debug.unshift(new jade.DebugItem( 2, jade_debug[0].filename ));
-buf.push("<div" + (jade.attr("style", maxWidth ? "max-width:" + (maxWidth) + "" : "", true, false)) + " class=\"container\">");
-jade_debug.unshift(new jade.DebugItem( undefined, jade_debug[0].filename ));
-jade_debug.unshift(new jade.DebugItem( 3, jade_debug[0].filename ));
 buf.push("<div class=\"message\">");
 jade_debug.unshift(new jade.DebugItem( undefined, jade_debug[0].filename ));
+jade_debug.unshift(new jade.DebugItem( 3, jade_debug[0].filename ));
+buf.push(null == (jade_interp = message) ? "" : jade_interp);
+jade_debug.shift();
+jade_debug.shift();
+buf.push("</div>");
+jade_debug.shift();
 jade_debug.unshift(new jade.DebugItem( 4, jade_debug[0].filename ));
-buf.push("<span class=\"accept\">");
+buf.push("<a href=\"javascript:;\" class=\"accept\">");
 jade_debug.unshift(new jade.DebugItem( undefined, jade_debug[0].filename ));
 jade_debug.unshift(new jade.DebugItem( 5, jade_debug[0].filename ));
 buf.push("<i class=\"fa fa-times\">");
@@ -25,22 +28,13 @@ jade_debug.shift();
 buf.push("</i>");
 jade_debug.shift();
 jade_debug.shift();
-buf.push("</span>");
-jade_debug.shift();
-jade_debug.unshift(new jade.DebugItem( 6, jade_debug[0].filename ));
-buf.push(null == (jade_interp = message) ? "" : jade_interp);
+buf.push("</a>");
 jade_debug.shift();
 jade_debug.shift();
 buf.push("</div>");
 jade_debug.shift();
-jade_debug.shift();
-buf.push("</div>");
-jade_debug.shift();
-jade_debug.shift();
-buf.push("</div>");
-jade_debug.shift();
-jade_debug.shift();}.call(this,"maxWidth" in locals_for_with?locals_for_with.maxWidth:typeof maxWidth!=="undefined"?maxWidth:undefined,"message" in locals_for_with?locals_for_with.message:typeof message!=="undefined"?message:undefined));;return buf.join("");
+jade_debug.shift();}.call(this,"message" in locals_for_with?locals_for_with.message:typeof message!=="undefined"?message:undefined));;return buf.join("");
 } catch (err) {
-  jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "#notification-bar\n    .container(style = maxWidth ? \"max-width:#{maxWidth}\" : \"\")\n        .message\n            span.accept\n                i.fa.fa-times\n            != message\n");
+  jade.rethrow(err, jade_debug[0].filename, jade_debug[0].lineno, "#notification-bar\n    .message\n        != message\n    a.accept(href='javascript:;')\n        i.fa.fa-times\n");
 }
 } }
