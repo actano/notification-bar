b676dfbdd2e05208e6b81f97e44516ad3086c669
decaffeinate <marcus.mennemeier@actano.de>
2016-11-12 10:21:17 +0100

    decaffeinate: Run post-processing cleanups on karma.conf.coffee

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,20 @@
-export default function(config) {
-    return config.set({
-        frameworks: ['mocha', 'chai'],
+// TODO: This file was created by bulk-decaffeinate.
+// Sanity-check the conversion and remove this comment.
+export default function (config) {
+  return config.set({
+    frameworks: ['mocha', 'chai'],
 
-        files: [
-            'out/component-build/build.js',
-            'out/component-build/build.css',
-            'lib/test/**/*.coffee',
-            {pattern: 'out/component-build/fortawesome/**/*', watched: false, included: false, served: true}
-        ],
+    files: [
+      'out/component-build/build.js',
+      'out/component-build/build.css',
+      'lib/test/**/*.coffee',
+            { pattern: 'out/component-build/fortawesome/**/*', watched: false, included: false, served: true },
+    ],
 
-        preprocessors: {
-            '**/*.coffee': ['coffee']
-        },
+    preprocessors: {
+      '**/*.coffee': ['coffee'],
+    },
 
-        singleRun: false
-    });
-};
+    singleRun: false,
+  })
+}
