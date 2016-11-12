cbd7a42b0b707e77d63be4e49cbc462fe79ab0b8
decaffeinate <marcus.mennemeier@actano.de>
2016-11-12 10:21:16 +0100

    decaffeinate: Convert karma.conf.coffee to JS

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,18 @@
-module.exports = (config) ->
-    config.set
-        frameworks: ['mocha', 'chai']
+export default function(config) {
+    return config.set({
+        frameworks: ['mocha', 'chai'],
 
         files: [
-            'out/component-build/build.js'
-            'out/component-build/build.css'
-            'lib/test/**/*.coffee'
+            'out/component-build/build.js',
+            'out/component-build/build.css',
+            'lib/test/**/*.coffee',
             {pattern: 'out/component-build/fortawesome/**/*', watched: false, included: false, served: true}
-        ]
+        ],
 
-        preprocessors:
+        preprocessors: {
             '**/*.coffee': ['coffee']
+        },
 
         singleRun: false
+    });
+};
